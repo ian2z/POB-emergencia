261cf77c45470834e1b13284c88ccbf8820d23aa
Josef Ian <josefian2016@gmail.com>
2026-03-26 17:23:56 -0300

    modificando Util e adicionando listagem

--- src/appconsole/Listar.java
+++ src/appconsole/Listar.java
@@ -1,4 +1,58 @@
 package appconsole;
 
+import java.util.List;
+
+import com.db4o.ObjectContainer;
+import com.db4o.query.Query;
+
+import modelo.Atendimento;
+import modelo.Paciente;
+import modelo.Upa;
+import util.Util;
+
 public class Listar {
+
+    public Listar(){
+        Util.conectarBanco();
+        ObjectContainer manager = Util.();
+
+        Query q;
+
+        System.out.println("\n---listagem de pacientes:");
+        q = manager.query();
+        q.constrain(Paciente.class);
+        List<Atendimento> atendimentos = q.execute();
+        for(Atendimento a: atendimentos){
+            System.out.println(a);
+        }
+
+
+        System.out.println("\n---listagem de motores:");
+        q = manager.query();
+        q.constrain(Motor.class);
+        List<Motor> motores = q.execute();
+        for(Motor m: motores){
+            System.out.println(m);
+        }
+
+
+        System.out.println("\n---listagem de motoristas:");
+        q = manager.query();
+        q.constrain(Motorista.class);
+        List<Motorista> motoristas = q.execute();
+        for(Motorista m: motoristas){
+            System.out.println(m);
+        }
+        Util.desconectar();
+
+        System.out.println("\n aviso: feche sempre o plugin OME antes de executar aplicação");
+    }
+
+
+
+    //=================================================
+    public static void main(String[] args) {
+        new Listar();
+    }
 }
+
--- src/util/Util.java
+++ src/util/Util.java
@@ -5,15 +5,18 @@ import javax.swing.JOptionPane;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
+import com.db4o.cs.Db4oClientServer;
 import com.db4o.config.EmbeddedConfiguration;
 
 // IMPORTANTE: Importe suas classes de modelo
+import com.db4o.cs.config.ClientConfiguration;
 import modelo.Atendimento;
 import modelo.Paciente;
 import modelo.Upa;
 
 public class Util {
     private static ObjectContainer manager;
+    private static String ipservidor;
 
     public static ObjectContainer conectarBanco() {
         if (manager != null)
@@ -52,6 +55,44 @@ public class Util {
         if (manager != null) {
             manager.close();
             manager = null;
+        }
+    }
+
+    private static void conectarBancoRemoto() {
+        if (manager != null)
+            return ; // ja tem uma conexao
+
+        // ---------------------------------------
+        // configurar e conectar banco remoto
+        // ---------------------------------------
+        EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+        config.common().messageLevel(0); // 0,1,2,3...
+
+        // Paciente
+        config.common().objectClass(Paciente.class).cascadeOnUpdate(true);
+        config.common().objectClass(Paciente.class).cascadeOnActivate(true);
+        config.common().objectClass(Paciente.class).cascadeOnDelete(false);
+
+        // Upa
+        config.common().objectClass(Upa.class).cascadeOnUpdate(true);
+        config.common().objectClass(Upa.class).cascadeOnActivate(true);
+        config.common().objectClass(Upa.class).cascadeOnDelete(false);
+
+        // Atendimento
+        config.common().objectClass(Atendimento.class).cascadeOnUpdate(true);
+        config.common().objectClass(Atendimento.class).cascadeOnActivate(true);
+        config.common().objectClass(Atendimento.class).cascadeOnDelete(false);
+
+        // **************************************
+        // Conexão client-server
+        // **************************************
+        try {
+            manager = Db4oClientServer.openClient((ClientConfiguration) config, ipservidor, 34000, "usuario1", "senha1");
+            //System.out.println("conectado ao banco " + manager);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao conectar ao banco remoto ip=" + ipservidor + "\n" + e.getMessage());
+            System.exit(0);
         }
     }
 }
