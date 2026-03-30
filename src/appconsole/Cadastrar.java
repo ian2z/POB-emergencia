0e3344afae5cd2e456843c2b3c7d9446f979a3e1
Josef Ian <josefian2016@gmail.com>
2026-03-30 20:49:07 -0300

    fix:registro de id e cadastro de atendimento e upa

--- src/appconsole/Cadastrar.java
+++ src/appconsole/Cadastrar.java
@@ -24,39 +24,40 @@ public class Cadastrar {
 
         paciente = new Paciente("13183543400", "João");
         mangabeira = new Upa("Mangabeira");
-        atendimento = new Atendimento(1, "29-03-2026", paciente, mangabeira);
+        atendimento = new Atendimento("29-03-2026", paciente, mangabeira);
+
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("11111111111", "Fulano da Silva");
-        atendimento = new Atendimento(2, "08-02-2026", paciente, mangabeira);
+        atendimento = new Atendimento("08-02-2026", paciente, mangabeira);
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("15654836502", "Antony Soprano");
         bessa = new Upa("Bessa");
-        atendimento = new Atendimento(3, "10-02-2026", paciente, bessa);
+        atendimento = new Atendimento("10-02-2026", paciente, bessa);
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("65896713506", "Maria do Carmo");
-        atendimento = new Atendimento(4, "23-01-2026", paciente, mangabeira);
+        atendimento = new Atendimento("23-01-2026", paciente, mangabeira);
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("68725461364", "Helena de Souza");
-        atendimento = new Atendimento(5, "01-01-2026", paciente, bessa);
+        atendimento = new Atendimento("01-01-2026", paciente, bessa);
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("22222222222", "teste");
-        atendimento = new Atendimento(5, "01-01-2026", paciente, bessa);
+        atendimento = new Atendimento("01-01-2026", paciente, bessa);
         manager.store(atendimento);
         manager.commit();
 
         paciente = new Paciente("46851236577", "Sicrano de Torres");
         bancarios = new Upa("Bancários");
-        atendimento = new Atendimento(5, "01-01-2026", paciente, bancarios);
+        atendimento = new Atendimento("01-01-2026", paciente, bancarios);
         manager.store(atendimento);
         manager.commit();
 
